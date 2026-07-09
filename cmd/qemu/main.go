a0a4aef95e6eb1b8348574a1bc1511e7a0df3116
Mateus Souza <mateussouzaweb@gmail.com>
2026-07-09 19:43:08 +0000

    Version 1.0.5

--- cmd/qemu/main.go
+++ cmd/qemu/main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/mateussouzaweb/libvirt-hooks/packages/system"
 )
 
-var version = "1.0.4"
+var version = "1.0.5"
 
 // PrintHelp prints usage information for the command-line tool
 func PrintHelp() error {
